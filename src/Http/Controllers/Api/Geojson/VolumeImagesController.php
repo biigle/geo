716b06a16d31227b9a5309c3bc0ea1f668092405
Sagun <sagunshre@gmail.com>
2020-05-31 19:46:16 +0200

    In VolumeImagesController#index action:
    Used 'join' instead of 'with' for building query to get images labels.
    Then grouped images by id and further grouped the images by label name and counted the number of image for each label name

--- src/Http/Controllers/Api/Geojson/VolumeImagesController.php
+++ src/Http/Controllers/Api/Geojson/VolumeImagesController.php
@@ -10,25 +10,27 @@ use League\Flysystem\FileNotFoundException;
 
 class VolumeImagesController extends Controller
 {
-  public function index($id){
+  public function index($id)
+  {
     $volume = Volume::findOrFail($id);
     $this->authorize('access', $volume);
 
-    $images = Image::where('volume_id', $id)->with(['annotations'=>function($a){
-      $a->with(['labels'=>function($al){
-        $al->with(['label'=>function($l){
-          $l->select('name','id');
-        }])->select('id', 'annotation_id', 'label_id');
-      }])->select('id', 'image_id');
-    }])->select('id', 'lat', 'lng')->get()->sortBy('id');
+    $images = $volume->images()->select('id', 'lat', 'lng');
+    $labels = $images->join('annotations', 'annotations.image_id', '=', 'images.id')
+      ->join('annotation_labels', 'annotation_labels.annotation_id', '=', 'annotations.id')
+      ->join('labels', 'labels.id', '=', 'annotation_labels.label_id')
+      ->select('images.id', 'images.lat', 'images.lng','labels.name')->get()
+      ->groupBy('id')->map(function($item, $key){
+        return $item->groupBy('name')->map(function($v){
+          return $v->count();
+        });
+      });
 
-    $features = $images->map(function($image){
-      $labels = $image->annotations->pluck('labels')->flatten()->pluck('label');
-      $feature = new \GeoJson\Feature\Feature(new \GeoJson\Geometry\Point([$image->lng, $image->lat]), Array('labels'=>$labels));
+    $features = $images->get()->map(function($image) use($labels){
+      $feature = new \GeoJson\Feature\Feature(new \GeoJson\Geometry\Point([$image->lng, $image->lat]), $labels[$image->id]->all());
       return $feature;
     });
 
-    $geojson_feature_Collection = new \GeoJson\Feature\FeatureCollection($features->all());
-    return $geojson_feature_Collection;
+    return new \GeoJson\Feature\FeatureCollection($features->all());;
   }
 }
