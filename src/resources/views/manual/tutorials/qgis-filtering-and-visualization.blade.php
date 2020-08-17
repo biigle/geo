de8cbfee0152406600084d4642561e35382f73f2
Sagun <sagunshre@gmail.com>
2020-08-17 14:15:40 +0200

    grammer corrections in Manual and added Proportional Symbol section in filtering manual

--- src/resources/views/manual/tutorials/qgis-filtering-and-visualization.blade.php
+++ src/resources/views/manual/tutorials/qgis-filtering-and-visualization.blade.php
@@ -11,9 +11,52 @@
       The response data of GeoJSON Api endpoints also includes Attributes of each point. These attributes are viewable in QGIS Attribute Table and <a href="https://docs.qgis.org/3.10/en/docs/user_manual/working_with_vector/attribute_table.html">numerous operations can be performed</a>. The Filtered points are represented with different color on the map.
     </p>
     <p>
-      One of the possible operation would be filtering data points by labels that occur in the images. This can be done through Attribute table.
+      One of the possible operation would be filtering data points by labels that occur in the images. This can be done through Query Builder Interface.
     </p>
+    <h3>Query Builder Interface</h3>
+    <p><a href="https://docs.qgis.org/3.10/en/docs/user_manual/working_with_vector/vector_properties.html#query-builder">Query Builder</a> filters and displays resulting points on map. To use Query Builder,</p>
+    <ul>
+      <li>Right Click on the choosen Layer and select "Filter..." option</li>
+      <p class="text-center">
+        <a href="{{asset('vendor/geo/images/manual/qgis_filtering_6.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_6.png')}}" width="90%"></a>
+      </p>
+      <li>In the section "Provider Specific Filter Expression", query condition can be build as shown in the image below.</li>
+      <p class="text-center">
+        <a href="{{asset('vendor/geo/images/manual/qgis_filtering_8.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_8.png')}}" width="90%"></a>
+      </p>
+      <p class="text-center">
+        <a href="{{asset('vendor/geo/images/manual/qgis_filtering_9.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_9.png')}}" width="90%"></a>
+      </p>
+    </ul>
+
+    <h3>Proportional Symbol</h3>
+    <p>Another Visualization which is possible in QGIS is Proportional Symbol using Layer's Symbology size assistance. This changes the size of data point circle depending on the number of selected annotations that occurs in a image.</p>
+
+    <p>To create Proportional Symbol,</p>
+    <ol>
+      <li>Import the data in QGIS and select it in the Layer Panel.</li>
+      <li>Open Layer Styling Panel.</li>
+      <p class="text-center">
+        <a href="{{asset('vendor/geo/images/manual/styling_1.png')}}"><img src="{{asset('vendor/geo/images/manual/styling_1.png')}}" width="90%"></a>
+      </p>
+
+      <li>In the Styling panel, click on <a href="{{asset('vendor/geo/images/manual/mIconDataDefine.png')}}"><img src="{{asset('vendor/geo/images/manual/mIconDataDefine.png')}}"></a> beside "size" field and a dropdown will appear.</li>
+      <li>Select "Assistant..." from the dropdown to open Symbol Size panel.</li>
+      <p class="text-center">
+        <a href="{{asset('vendor/geo/images/manual/styling_2.png')}}"><img src="{{asset('vendor/geo/images/manual/styling_2.png')}}" width="90%"></a>
+      </p>
+      <li>Select a desired Label from "Source" dropdown based on which you wish to create proportional symbols and change the Inputs "Value from" and "to" to "1,000000" and "20,000000" respectively.</li>
+      <p class="text-center">
+        <a href="{{asset('vendor/geo/images/manual/styling_3.png')}}"><img src="{{asset('vendor/geo/images/manual/styling_3.png')}}" width="90%"></a>
+      </p>
+      <p class="text-center">
+        <a href="{{asset('vendor/geo/images/manual/styling_4.png')}}"><img src="{{asset('vendor/geo/images/manual/styling_4.png')}}" width="90%"></a>
+      </p>
+    </ol>
+    <p>To clear proportional symbol, In the Styling panel, click on <a href="{{asset('vendor/geo/images/manual/mIconDataDefine.png')}}"><img src="{{asset('vendor/geo/images/manual/mIconDataDefine.png')}}"></a> beside "size" field and select "clear" from dropdown option.</p>
+
     <h3>Attribute Table</h3>
+    <p>Attribute Table Selects Features based on the filter query. It does not filter the Feature data points, but merely selects Features and distinguishes those data points by changing their color. To use Attribute Table,</p>
     <p><a href="https://docs.qgis.org/3.10/en/docs/user_manual/working_with_vector/attribute_table.html#introducing-the-attribute-table-interface" target="_blank">Open the Attribute Table Interface</a> and click <a href="https://docs.qgis.org/3.10/en/docs/user_manual/working_with_vector/attribute_table.html#filtering-and-selecting-features-using-forms" target="_blank">Filter/Select feature using form</a>.</p>
     <p class="text-center">
         <a href="{{asset('vendor/geo/images/manual/qgis_filtering_1.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_1.png')}}" width="90%"></a>
@@ -21,7 +64,7 @@
     <p class="text-center">
         <a href="{{asset('vendor/geo/images/manual/qgis_filtering_2.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_2.png')}}" width="90%"></a>
     </p>
-    <p>The form with the label attributes are visible. We can set the value of the. Now to filter the images which includes Crustasean label greater than 2 and does not have Sponge Label</p>
+    <p>The form with the label attributes are visible. We can set the value of the. Now to select images which includes Crustasean label greater than 2 and does not have Sponge Label.</p>
     <ul>
       <li>Set Crustasean input field to 2 and select "Greater than (>)" from the drop down</li>
       <p class="text-center">
@@ -37,20 +80,5 @@
       </p> 
     </ul> 
     <p>The Yellow colored points are the images with Crustasean label greater than 2 and does not have Sponge Label.</p>
-    <h3>Query Builder Interface</h3>
-    <p>Another way of filtering option in QGIS is using <a href="https://docs.qgis.org/3.10/en/docs/user_manual/working_with_vector/vector_properties.html#query-builder">Query Builder</a>, which filters and displays only resulting points on map.</p>
-    <ul>
-      <li>Right Click on the choosen Layer and select "Filter..." option</li>
-      <p class="text-center">
-        <a href="{{asset('vendor/geo/images/manual/qgis_filtering_6.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_6.png')}}" width="90%"></a>
-      </p>
-      <li>In the section "Provider Specific Filter Expression", query condition can be build as shown in the image below.</li>
-      <p class="text-center">
-        <a href="{{asset('vendor/geo/images/manual/qgis_filtering_8.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_8.png')}}" width="90%"></a>
-      </p>
-      <p class="text-center">
-        <a href="{{asset('vendor/geo/images/manual/qgis_filtering_9.png')}}"><img src="{{asset('vendor/geo/images/manual/qgis_filtering_9.png')}}" width="90%"></a>
-      </p>
-    </ul>
   </div>
 @endsection
--- src/resources/views/manual/tutorials/qgis-getting-started.blade.php
+++ src/resources/views/manual/tutorials/qgis-getting-started.blade.php
@@ -4,16 +4,16 @@
 
 @section('manual-content')
   <div class="row">
-    <p class="lead">Introduction on Interfacing Biigle Api with QGis.</p>
+    <p class="lead">Introduction on Interfacing BIIGLE API with QGIS.</p>
     <p>
-      QGis is an open source Geographic Information System that is used for viewing, editing and analysis of geospatial data. In order to interface Biigle with QGis, API endpoints, which respond with GeoJSON format Data, had been added. These API’s can only be accessed by Project Editor, Expert and Admins.
+      QGIS is an open source Geographic Information System that is used for viewing, editing and analysis of geospatial data. In order to interface BIIGLE with QGIS, API endpoints, which respond with GeoJSON format data, had been added. These API’s can only be accessed by all the members of the Project.
     </p>
 
     <p>
-      The GeoJSON Format data From the API can be imported to QGIS by creating a new Vector Layer in QGis. In the following examples you will learn how to Successfully configure the Authentication Credential for Biigle account in QGIS, add a Vector Layer and import Images data points of a volume whose id is specified in Api URI.
+      The GeoJSON format data From the API can be imported to QGIS by creating a new "Vector Layer" in QGIS. In the following examples you will learn how to successfully configure the Authentication Credential for BIIGLE account in QGIS, add a "Vector Layer" and import Images data points of a volume whose id is specified in API URI.
     </p>
-    <p><i>Note: For this manual, QGIS Version 3.14 is being used.</i></p>
-    <p>First, Open QGIS Desktop and Click on the "Layer" option in Menu.</p>
+    <p><i>Note: For this manual, QGIS Version 3.10 (Long Term Release) is being used.</i></p>
+    <p>First, Open QGIS Desktop and click on the "Layer" option in Menu.</p>
     <p class="text-center">
         <a href="{{asset('vendor/geo/images/manual/QGIS_1.jpeg')}}"><img src="{{asset('vendor/geo/images/manual/QGIS_1.jpeg')}}" width="100%" style="border: 1px solid #111;"></a>
     </p>
@@ -25,13 +25,13 @@
     <p class="text-center">
         <a href="{{asset('vendor/geo/images/manual/QGIS_3.jpeg')}}"><img src="{{asset('vendor/geo/images/manual/QGIS_3.jpeg')}}" width="100%" style="border: 1px solid #111;"></a>
     </p>
-    <p>In the Dialog box,
-      <ul>
+    <p>In the Dialog Box,
+      <ol>
         <li>select “Protocol, HTTP(s), cloud, etc” for Source Type.</li>
         <li> Leave the Encoding to “Automatic”</li>
         <li>In Protocol Type select “GeoJSON”</li>
         <li> Added Volume Images API URI "http://biigle.de/api/v1/geojson/volumes/1/images</li>
-      </ul>
+      </ol>
     </p>
     <p class="text-center">
         <a href="{{asset('vendor/geo/images/manual/QGIS_4.jpeg')}}"><img src="{{asset('vendor/geo/images/manual/QGIS_4.jpeg')}}" width="100%" style="border: 1px solid #111;"></a>
@@ -39,8 +39,8 @@
     <p>In the Authentication Section, Click on “+” button. This Opens Authentication Dialog Box.
       <ul>
         <li>The “Name” field is optional.</li>
-        <li>In the Username field type Biigle account email address. Note: replace “@” in the email with “%40”.</li>
-        <li>Then, Generate a new Biigle API token from Settings -> <a href="http://biigle.de/settings/tokens">Token page</a> and add the token in the Password field.</li>
+        <li>In the Username field type BIIGLE account email address. Note: replace “@” in the email with “%40”.</li>
+        <li>Then, generate a new BIIGLE API token from settings -> <a href="http://biigle.de/settings/tokens">Token page</a> and add the token in the password field.</li>
         <li>Click Save.</li>
       </ul>
     </p>
@@ -53,17 +53,6 @@
     <p>Select the newly added Authentication configuration from dropdown and click Add.</p>
     <p class="text-center">
       <a href="{{asset('vendor/geo/images/manual/QGIS_7.jpeg')}}"><img src="{{asset('vendor/geo/images/manual/QGIS_7.jpeg')}}" width="100%" style="border: 1px solid #111;"></a>
-    </p>
-    <p>You can add an Open street Map to the layer. In the Browser Section, right click on “OpenStreetMap” under “XYZ Tiles”.</p>
-    <p class="text-center">
-      <a href="{{asset('vendor/geo/images/manual/QGIS_8.jpeg')}}"><img src="{{asset('vendor/geo/images/manual/QGIS_8.jpeg')}}" width="100%" style="border: 1px solid #111;"></a>
-    </p>
-    <p>In the Layer section, right click on “openstreetmap” and select “Move to Bottom”</p>
-    <p class="text-center">
-      <a href="{{asset('vendor/geo/images/manual/QGIS_9.jpeg')}}"><img src="{{asset('vendor/geo/images/manual/QGIS_9.jpeg')}}" width="100%" style="border: 1px solid #111;"></a>
-    </p>
-    <p class="text-center">
-      <a href="{{asset('vendor/geo/images/manual/QGIS_10.jpeg')}}"><img src="{{asset('vendor/geo/images/manual/QGIS_10.jpeg')}}" width="100%" style="border: 1px solid #111;"></a>
     </p>
   </div>
 @endsection
