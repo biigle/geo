98fb6c6bbb0895a5b3d34a4c236298245e6ccb8c
Max Tiessen <max.tiessen@uni-bielefeld.de>
2023-11-16 16:39:27 +0100

    Added dependency for new filter

--- src/resources/assets/js/volumes/import.js
+++ src/resources/assets/js/volumes/import.js
@@ -1,2 +1,3 @@
 export let VolumeFilters = biigle.$require('volumes.stores.filters');
 export let FilterList = biigle.$require('volumes.components.filterListComponent');
+export let VolumesApi = biigle.$require('annotations.api.volumes');
