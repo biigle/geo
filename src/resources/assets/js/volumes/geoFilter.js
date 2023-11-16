a6a2b6b67932150a88f1cb19011fe5ee5b3d3d23
Max Tiessen <max.tiessen@uni-bielefeld.de>
2023-11-16 16:40:31 +0100

    Add new goeFilter to filter-list.

--- src/resources/assets/js/volumes/geoFilter.js
+++ src/resources/assets/js/volumes/geoFilter.js
@@ -1,5 +1,9 @@
 import {VolumeFilters} from './import';
 import {FilterList} from './import';
+import {VolumesApi} from './import';
+import FilterSelect from '../geo/components/filterByLocationComponent';
+import {handleErrorResponse} from '../../../../../../../../resources/assets/js/core/messages/store';
+
 
 /**
  * Geo filter for the volume overview filters.
@@ -29,5 +33,53 @@ if (Array.isArray(VolumeFilters)) {
 
             return new Vue.Promise.resolve({data});
         },
+    });
+
+    VolumeFilters.push({
+        id: 'location',
+        types: ['image'],
+        label: 'filter by location',
+        help: "All images that were (not) selected on the world map.",
+        listComponent: {
+            mixins: [FilterList],
+            data() {
+                return {
+                    name: 'filter by location',
+                };
+            },
+            created() {
+                window.addEventListener('storage', () => {
+                    this.$emit('refresh', this.rule);
+                });
+            },
+        },
+        selectComponent: {
+            mixins: [FilterSelect],
+            components: {
+            },
+            data() {
+                return {
+                    placeholder: 'Label name',
+                    text: "this is an example text",
+                };
+            },
+            created() {
+                VolumesApi.queryFilenames({id: this.volumeId})
+                    .then(this.retrieveFiles, handleErrorResponse);
+            },
+        },
+        getSequence(volumeId) {
+            let key = 'biigle.geo.imageSequence.' + volumeId;
+            let data = JSON.parse(localStorage.getItem(key)) || [];
+
+            return new Vue.Promise.resolve({data});
+        },
+        // // add imageIds as variable
+        // getSequence(volumeId) {
+        //     let key = 'biigle.geo.imageSequence.' + volumeId;
+        //     let data = JSON.parse(localStorage.getItem(key)) || [];
+
+        //     return new Vue.Promise.resolve({data});
+        // },
     });
 }
