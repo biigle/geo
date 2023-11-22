a71b95875b9326a22c94258e7e28213721c2a7ac
Max Tiessen <max.tiessen@uni-bielefeld.de>
2023-11-22 15:45:17 +0100

    add basic geoOverlay model and controller

---
+++ src/geoOverlay.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Biigle;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class geoOverlay extends Model
+{
+    use HasFactory;
+}
