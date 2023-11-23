444cbd1f9f31d658695e92b5f795775de788b84c
Max Tiessen <max.tiessen@uni-bielefeld.de>
2023-11-23 09:44:19 +0100

    fill geoOverlay model with functions

--- src/geoOverlay.php
+++ src/geoOverlay.php
@@ -1,11 +1,85 @@
 <?php
 
-namespace Biigle;
+namespace Biigle\Modules\Geo;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+use Biigle\Volume;
+use Storage;
 
 class geoOverlay extends Model
 {
-    use HasFactory;
+    /**
+     * Don't maintain timestamps for this model.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes hidden in the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'volume_id',
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'top_left_lng' => 'float',
+        'top_left_lat' => 'float',
+        'bottom_right_lng' => 'float',
+        'bottom_right_lat' => 'float',
+    ];
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Delete the overlay image file when the model is deleted.
+        static::deleting(function ($overlay) {
+            Storage::disk(config('geo.overlay_storage_disk'))->delete($overlay->path);
+        });
+    }
+
+    /**
+     * The volume, this overlay belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function volume()
+    {
+        return $this->belongsTo(Volume::class);
+    }
+
+    /**
+     * Get the local path to the overlay image file.
+     *
+     * @return string
+     */
+    public function getPathAttribute()
+    {
+        return "{$this->volume_id}/{$this->id}";
+    }
+
+    /**
+     * Store the uploaded image file of the geo overlay.
+     *
+     * @param UploadedFile $file
+     */
+    public function storeFile(UploadedFile $file)
+    {
+        Storage::disk(config('geo.overlay_storage_disk'))
+            ->putFileAs($this->volume_id, $file, $this->id);
+    }
 }
