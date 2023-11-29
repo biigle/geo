2b4ebcd6c0d176490525a183a872216d24fa8e5c
Max Tiessen <max.tiessen@uni-bielefeld.de>
2023-11-29 16:49:25 +0100

    added some missing imports

--- src/resources/assets/js/volumes/import.js
+++ src/resources/assets/js/volumes/import.js
@@ -1,3 +1,5 @@
 export let VolumeFilters = biigle.$require('volumes.stores.filters');
 export let FilterList = biigle.$require('volumes.components.filterListComponent');
 export let LoaderMixin = biigle.$require('core.mixins.loader');
+export let EditorMixin = biigle.$require('core.mixins.editor');
+export let handleErrorResponse = biigle.$require('messages').handleErrorResponse;
