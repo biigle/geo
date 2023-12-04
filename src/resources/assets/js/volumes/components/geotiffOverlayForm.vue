89af5cf927dfc507d5e22f63fb1fd625734a85b2
Max Tiessen <max.tiessen@uni-bielefeld.de>
2023-12-04 18:01:57 +0100

    Include geotiff.js for testing

--- src/resources/assets/js/volumes/components/geotiffOverlayForm.vue
+++ src/resources/assets/js/volumes/components/geotiffOverlayForm.vue
@@ -2,6 +2,7 @@
 import Api from '../api/geoOverlays';
 import {handleErrorResponse} from '../import';
 import {LoaderMixin} from '../import';
+import GeoTIFF, {fromBlob} from 'geotiff';
 
 
 export default {
@@ -10,7 +11,10 @@ export default {
     ],
     data() {
         return {
-            selectedFile: null,
+            selectedTLLat: '',
+            selectedTLLng: '',
+            selectedBRLat: '',
+            selectedBRLng: '',
         };
     },
     methods: {
@@ -18,10 +22,25 @@ export default {
             this.$refs.geoTiffInput.click();
         },
 
-        uploadGeoTiff(event) {
+        async uploadGeoTiff(event) {
             this.startLoading();
-            this.selectedFile = event.target.files[0];
-            console.log("data: ", this.selectedFile);
+            let tiff = await fromBlob(event.target.files[0]);
+            // to obtain the most detailed image. 
+            let geoTiffImage = await tiff.getImage(0);
+            let bbox = geoTiffImage.getBoundingBox(); // [minx, miny, maxx, maxy]
+            console.log("data: ", tiff);
+            console.log("geoTiffImage: ", geoTiffImage);
+            console.log("bbox: ", bbox);
+            console.log("geoKeys: ", await geoTiffImage.getGeoKeys());
+            console.log("gdal-info: ", await geoTiffImage.getGDALMetadata());
+            let data = new FormData();
+            data.append('image', geoTiffImage);
+            data.append('top_left_lat', bbox[1]);
+            data.append('top_left_long', bbox[0]);
+            data.append('bottom_right_lat', bbox[3]);
+            data.append('bottom_right_long', bbox[2]);
+
+
             // this.upload(data)
             //     .then(this.handleSuccess, this.handleError)
             //     .finally(this.finishLoading);
