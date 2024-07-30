1c00d56db069adf65782b4ced1532c9cde1cdddb
Max T <max.tiessen@uni-bielefeld.de>
2024-07-30 15:59:15 +0200

    Added blueprint Migration-, Model-, Controller-files for WebMapOverlays

---
+++ src/Http/Controllers/Api/WebMapOverlayController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Biigle\Modules\Geo\Http\Controllers\Api;
+
+use Biigle\Modules\Geo\WebMapOverlay;
+use Illuminate\Http\Request;
+use Biigle\Http\Controllers\Api\Controller;
+
+
+class WebMapOverlayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WebMapOverlay $webMapOverlay)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WebMapOverlay $webMapOverlay)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, WebMapOverlay $webMapOverlay)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WebMapOverlay $webMapOverlay)
+    {
+        //
+    }
+}
