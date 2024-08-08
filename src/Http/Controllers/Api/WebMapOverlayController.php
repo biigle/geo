6b98f4024b15e2c9f26a1b4e98f4927aef2d3ab7
Max T <max.tiessen@uni-bielefeld.de>
2024-08-08 18:57:53 +0200

    Restructure the store method in WebMap Controller, making own class of helper functions. + small bugfix in uniqueUrl exception.

--- src/Http/Controllers/Api/WebMapOverlayController.php
+++ src/Http/Controllers/Api/WebMapOverlayController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Biigle\Http\Controllers\Api\Controller;
 use Biigle\Modules\Geo\Http\Requests\StoreWebMapOverlay;
 use Biigle\Modules\Geo\Http\Requests\UpdateOverlay;
+use Biigle\Modules\Geo\Services\Support\WebMapSource;
 use Illuminate\Validation\ValidationException;
 
 class WebMapOverlayController extends Controller
@@ -33,65 +34,45 @@ class WebMapOverlayController extends Controller
     public function store(StoreWebMapOverlay $request)
     {
         $validated = $request->validated();
-        $webmapUrl = $request->input('url');
         $volumeId = $request->input('volumeId');
-        $parsed_url = parse_url($webmapUrl);
+        $webmapSource = new WebMapSource($request->input('url'));
 
         // Check whether provided url is base url or query-url
         // 1. Case --> base URL given
         // Steps: perform getCapablities lookup and extract first layer
-        if(is_null(parse_url($webmapUrl, PHP_URL_QUERY))) {
-            $baseUrl = $this->unparseUrl($parsed_url);
-            $xmlResult = $this->getCapabilities($baseUrl);
-            [$webmapTitle, $webmapLayers] = $this->firstValidLayer($xmlResult);
-            $overlay = $this->saveWebMapOverlay($volumeId, $baseUrl, $webmapTitle, $webmapLayers);
+        if(!$webmapSource->isQueryUrl()) {
+            [$webmapTitle, $webmapLayers] = $webmapSource->firstValidLayer();
+            $overlay = $this->saveWebMapOverlay($volumeId, $webmapSource->baseUrl, $webmapTitle, $webmapLayers);
         } else {
             // 2. Case --> specific URL with query parameters given (e.g. '?service=wms&version=1.1.0&request=GetMap&layers=CV_Acc...')
             // Steps: Extract layer from layers parameter in url
-            $baseUrl = $this->unparseUrl($parsed_url, $base = true);
-            $xmlResult = $this->getCapabilities($baseUrl);
-            $queryString = $parsed_url['query'];
-            
-            // split the query-string into its compartments
-            parse_str(urldecode($queryString), $output);
-            // if queryString is empty or does not contain the layers parameter, use getCapabilities-request to find layer (like first case)
-            if(empty($queryString) || empty($output['layers'])) {
-                [$webmapTitle, $webmapLayers] = $this->firstValidLayer($xmlResult);
+            $webmapLayers = $webmapSource->extractLayersFromQueryUrl();
+            // if no layers could be found in the url query-string
+            if(is_null($webmapLayers)) {
+                // use getCapabilities-request to find layer (like first case)
+                [$webmapTitle, $webmapLayers] = $webmapSource->firstValidLayer();
             } else {
-                // Extract layers from url query-string
-                $layerString = $output['layers'];
-                // if multiple layers are defined in url layers-parameter:
-                if(str_contains($layerString, ',')) {
-                    $webmapLayers = explode(',', $layerString);
-                    // overwrite layerString with only first layer (searches for title of this layer in getCapabilities)
-                    $layerString = $webmapLayers[0];
-                } else {
-                    // if $layerString contains only one layer
-                    $webmapLayers = [$layerString];
-                }
-                
-                // xpath query to find the corresponding layer-title in the getCapabilities xml
-                $titleArray = $xmlResult->xpath('(//*[local-name()="Layer"]/*[Name="' . $layerString .'"])[1]/Title');
-                if(count($titleArray) !== 0) {
-                    $webmapTitle = (string) $titleArray[0];
-                } else { // default case
-                    $webmapTitle = $layerString;
-                }
+                // overwrite layerString with only first layer (search for title of this layer in getLayerTitle)
+                $firstLayerName = $webmapLayers[0];
+                    // get corresponding layer-title from webmapSource
+                $webmapTitle = $webmapSource->getLayerTitle($firstLayerName);
             }
 
-            // check whether baseURL exists alread in DB
-            $existingUrls = WebMapOverlay::where('volume_id', $volumeId)->pluck('url')->toArray();
-            if (in_array($baseUrl, $existingUrls)) {
+            // check whether baseURL exists already in DB
+            $existingWms = WebMapOverlay::where('volume_id', $volumeId)->pluck('url', 'name')->all();
+            // $existingUrls
+            if (in_array($webmapSource->baseUrl, array_values($existingWms))) {
                 // strip the url if too long
-                $urlShort = strlen($baseUrl) > 80 ? substr($baseUrl, 0, 80) . "..." : $baseUrl;
+                $urlShort = strlen($webmapSource->baseUrl) > 80 ? substr($webmapSource->baseUrl, 0, 80) . "..." : $webmapSource->baseUrl;
+                $wmsTitle = array_search($webmapSource->baseUrl, $existingWms);
                 throw ValidationException::withMessages(
                     [
-                        'uniqueUrl' => ["The url \"{$urlShort}\" has already been uploaded (Filename: \"{$webmapTitle}\")."],
+                        'uniqueUrl' => ["The url \"{$urlShort}\" has already been uploaded (Filename: \"{$wmsTitle}\")."],
                     ]
                 );
             }
             // save the WebMapOverlay to DB
-            $overlay = $this->saveWebMapOverlay($volumeId, $baseUrl, $webmapTitle, $webmapLayers);
+            $overlay = $this->saveWebMapOverlay($volumeId, $webmapSource->baseUrl, $webmapTitle, $webmapLayers);
         }
         
         return $overlay;
@@ -121,73 +102,7 @@ class WebMapOverlayController extends Controller
 
         return $overlay;
     }
-
-    protected function firstValidLayer($xmlResult)
-    {
-        // select only those layers that have no Child layers within them
-        $layers = $xmlResult->xpath('//*[local-name()="Layer"][not(.//*[local-name()="Layer"])]');
-        // loop over layers and return first valid layer title and name
-        foreach($layers as $layer) {
-            if((string) $layer['queryable'] === "1") {
-                $webmapTitle = (string) $layer->Title;
-                // Excerpt from OpenGIS 'Web Map Server Implementation Specification':
-                // If, and only if, a layer has a <Name>, then it is a map layer that can be requested
-                // If the layer has a Title but no Name, then that layer is only a category title for
-                // all the layers nested within (the latter case should not occur due to xpath query above)
-                if(!empty($layer->Name)) {
-                    $webmapLayers = [(string) $layer->Name];
-                    return [$webmapTitle, $webmapLayers];
-                }
-            }
-        }
-        throw ValidationException::withMessages(
-            [
-                'noValidLayer' => ["Could not find any valid layers within the WMS resource."],
-            ]
-        );
-    }
     
-    /**
-     * Checks whether the base-url is an actual WMS resource and returns 'getCapabilities' XML
-     */
-    protected function getCapabilities($baseUrl)
-    {
-        $wmsRequest = file_get_contents($baseUrl.'?service=wms&version=1.1.1&request=GetCapabilities');
-        libxml_use_internal_errors(true); // suppress all XML errors
-        $xml = simplexml_load_string($wmsRequest);
-        if($xml === false) {
-            throw ValidationException::withMessages(
-                [
-                    'invalidWMS' => ["The url does not lead to a WMS resource."],
-                ]
-            );
-        }
-        return $xml;
-    }
-
-    /**
-     * Re-assemble a parsed url
-     */
-    protected function unparseUrl($parsed_url, $base = false)
-    {
-        // return only the base url
-        if($base == true) {
-            unset($parsed_url['fragment']);
-            unset($parsed_url['query']);
-        }
-
-        $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-        $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-        $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-        $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-        $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
-        $pass     = ($user || $pass) ? "$pass@" : '';
-        $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-        $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
-
-        return "$scheme$user$pass$host$port$path$query$fragment";
-    }
 
     /**
      * Display the specified resource.
