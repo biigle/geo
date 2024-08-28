d7f003fdaada2206be375a8f862181e9a8516dbc
Max T <max.tiessen@uni-bielefeld.de>
2024-08-28 17:35:35 +0200

    Add .tiff to supported types in geoTiff-form

--- src/resources/views/volumes/edit/geotiffOverlayForm.blade.php
+++ src/resources/views/volumes/edit/geotiffOverlayForm.blade.php
@@ -2,7 +2,7 @@
     <p>Upload a geo overlay in geoTIFF (.tif) format</p>
     <form class="form" v-on:submit.prevent="submitGeoTiff">
         <div class="form-group">
-            <input class="hidden" ref="geoTiffInput" type="file" name="file" v-on:change="uploadGeoTiff" accept=".tif">
+            <input class="hidden" ref="geoTiffInput" type="file" name="file" v-on:change="uploadGeoTiff" accept=".tif,.tiff">
             <button class="btn btn-default" type="submit" :disabled="loading">Upload geoTIFF</button>
         </div>
     </form>
