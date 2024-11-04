296f2a9858dfaba4d8f0fa47f3e99c85491c4ed2
Max T <max.tiessen@uni-bielefeld.de>
2024-11-04 17:17:45 +0100

    reapply color-band normalization

--- src/Jobs/TileSingleOverlay.php
+++ src/Jobs/TileSingleOverlay.php
@@ -57,55 +57,54 @@ class TileSingleOverlay extends TileSingleObject
         }
     }
 
-    //  /**
-    //  * Generate tiles for the object and put them to temporary storage.
-    //  *
-    //  * @param File $file
-    //  * @param string $path Path to the cached image file.
-    //  */
-    // public function generateTiles($file, $path)
-    // {
-    //     // $vipsImage = $this->getVipsImage($path);
-    //     // $min = $vipsImage->min();
-    //     // $max = $vipsImage->max();
-    //     // dd($min, $max);
+     /**
+     * Generate tiles for the object and put them to temporary storage.
+     *
+     * @param File $file
+     * @param string $path Path to the cached image file.
+     */
+    public function generateTiles($file, $path)
+    {
+        $vipsImage = $this->getVipsImage($path);
+        $min = $vipsImage->min();
+        $max = $vipsImage->max();
 
-    //     // if($min < 0 || $max < 0 || $max > 255) {
-    //     //     $vipsImage = $this->imageNormalization($vipsImage, $min, $max);
-    //     //     $vipsImage->dzsave($this->tempPath, [
-    //     //         'layout' => 'zoomify',
-    //     //         'container' => 'fs',
-    //     //         'strip' => true,
-    //     //     ]);
-    //     // } else {
-    //         parent::generateTiles($file, $path);
-    //     // }
-    // }
 
-    // /**
-    //  * Get the vips image instance.
-    //  *
-    //  * @param string $path
-    //  *
-    //  * @return \Jcupitt\Vips\Image
-    //  */
-    // protected function getVipsImage($path)
-    // {
-    //     return VipsImage::newFromFile($path);
-    // }
+        if($min < 0 || $min > 255 || $max < 0 || $max > 255) {
+            $this->imageNormalization($vipsImage, $min, $max)->dzsave($this->tempPath, [
+                'layout' => 'zoomify',
+                'container' => 'fs',
+                'strip' => true,
+            ]);
+        } else {
+            parent::generateTiles($file, $path);
+        }
+    }
 
-    // /**
-    //  * Normalize the image band to 0 to 255
-    //  * 
-    //  * @param \Jcupitt\Vips\Image $vipsImage
-    //  * @param float $min minimum value of color-level of the input image
-    //  * @param float $max maximum value of color-level of the input image
-    //  * 
-    //  * @return \Jcupitt\Vips\Image
-    //  */
-    // protected function imageNormalization($vipsImage, $min, $max)
-    // {
-    //     // band intensity normalization x' = (x - $min) / ($max - $min) * 255
-    //     return $vipsImage->subtract($min)->divide($max - $min)->multiply(255.0);
-    // }
+    /**
+     * Get the vips image instance.
+     *
+     * @param string $path
+     *
+     * @return \Jcupitt\Vips\Image
+     */
+    protected function getVipsImage($path)
+    {
+        return VipsImage::newFromFile($path);
+    }
+
+    /**
+     * Normalize the image band to 0 to 255
+     * 
+     * @param \Jcupitt\Vips\Image $vipsImage
+     * @param float $min minimum value of color-level of the input image
+     * @param float $max maximum value of color-level of the input image
+     * 
+     * @return \Jcupitt\Vips\Image
+     */
+    protected function imageNormalization($vipsImage, $min, $max)
+    {
+        // band intensity normalization x' = (x - $min) / ($max - $min) * 255
+        return $vipsImage->subtract($min)->divide($max - $min)->multiply(255);
+    }
 }
