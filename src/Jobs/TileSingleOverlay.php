812c6791fc1155481ab4a5f9626a004c44503066
Leane Schlundt <lhecht@techfak.de>
2025-10-16 10:47:44 +0200

    Create static functions to enable testing

--- src/Jobs/TileSingleOverlay.php
+++ src/Jobs/TileSingleOverlay.php
@@ -67,11 +67,11 @@ class TileSingleOverlay extends TileSingleObject
     {
         $vipsImage = $this->getVipsImage($path);
         // exclude the NoData values (-99999) of the geoTIFF file when searching the min
-        $min = $vipsImage->equal(-99999)->ifthenelse(0, $vipsImage)->min();
-        $max = $vipsImage->max();
+        $min = self::min($vipsImage);
+        $max = self::max($vipsImage);
 
         if($min < 0 || $min > 255 || $max < 0 || $max > 255) {
-            $this->imageNormalization($vipsImage, $min, $max)->dzsave($this->tempPath, [
+            self::imageNormalization($vipsImage, $min, $max)->dzsave($this->tempPath, [
                 'layout' => 'zoomify',
                 'container' => 'fs',
                 'strip' => true,
@@ -102,9 +102,33 @@ class TileSingleOverlay extends TileSingleObject
      * 
      * @return \Jcupitt\Vips\Image
      */
-    protected function imageNormalization($vipsImage, $min, $max)
+    public static function imageNormalization($vipsImage, $min, $max)
     {
         // band intensity normalization x' = (x - $min) / ($max - $min) * 255
         return $vipsImage->subtract($min)->multiply(255 / ($max - $min));
+    }
+
+    /**
+     * Return smallest pixel value
+     *
+     * @param VipsImage $img
+     *
+     * @return int
+     */
+    public static function min($img)
+    {
+        return $img->equal(-99999)->ifthenelse(0, $img)->min();
+    }
+
+    /**
+     * Return largest pixel value
+     *
+     * @param VipsImage $img
+     *
+     * @return int
+     */
+    public static function max($img)
+    {
+        return $img->max();
     }
 }
