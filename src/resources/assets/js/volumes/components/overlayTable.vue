b625dd9d6dc67ba8eb4282f1727cbe6b4d0f5ef3
Leane Schlundt <lhecht@techfak.de>
2025-10-22 11:06:54 +0200

    Fix missing entries

--- src/resources/assets/js/volumes/components/overlayTable.vue
+++ src/resources/assets/js/volumes/components/overlayTable.vue
@@ -15,8 +15,8 @@
             </thead>
             <draggable v-model="sortedOverlays" tag="tbody" handle=".handle" item-key="id">
                 <template #item="{ element: overlay, index }">
-                    <tr is="overlay-item" :key="overlay.id" :class="{'list-group-item-success': overlay.isNew}" :index="index" :overlay="overlay" :volume-id="volumeId" v-on:remove="$emit('remove', overlay);">
-                    </tr>
+                    <overlay-item :key="overlay.id" :class="{'list-group-item-success': overlay.isNew}" :index="index" :overlay="overlay" :volume-id="volumeId" v-on:remove="$emit('remove', overlay);">
+                    </overlay-item >
                 </template>
             </draggable>
         </table>
