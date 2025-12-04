eb617210d39833cd12fe2b308343957892b82871
Leane Schlundt <lhecht@techfak.de>
2025-12-04 11:08:13 +0100

    Add down method to migration

--- src/Database/migrations/2025_12_04_085344_edit_geo_overlays.php
+++ src/Database/migrations/2025_12_04_085344_edit_geo_overlays.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -22,6 +21,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('geo_overlays', function (Blueprint $table) {
+            $table->column('context_layer')->default(0);
+            $table->boolean('browsing_layer')->default(0)->change();
+        });
     }
 };
