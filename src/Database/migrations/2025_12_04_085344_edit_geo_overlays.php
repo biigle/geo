6eddc05049102f77b84078740a2c2d965c8fb8cd
Leane Schlundt <lhecht@techfak.de>
2025-12-04 11:07:05 +0100

    Revert "Fix migration not updating column"
    This reverts commit 2ea490dc668ffdc9a1e4d565f7128724c473d4c6.

--- src/Database/migrations/2025_12_04_085344_edit_geo_overlays.php
+++ src/Database/migrations/2025_12_04_085344_edit_geo_overlays.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,9 +13,6 @@ return new class extends Migration {
     {
         Schema::table('geo_overlays', function (Blueprint $table) {
             $table->dropColumn('context_layer');
-        });
-
-        Schema::table('geo_overlays', function (Blueprint $table) {
             $table->boolean('browsing_layer')->default(1)->change();
         });
     }
@@ -24,12 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('geo_overlays', function (Blueprint $table) {
-            $table->boolean('context_layer')->default(0);
-        });
-
-        Schema::table('geo_overlays', function (Blueprint $table) {
-            $table->boolean('browsing_layer')->default(0)->change();
-        });
+        //
     }
 };
