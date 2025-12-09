4f168c3ff157da0d0bd2ae26bbce37385a250c8e
Leane Schlundt <lhecht@techfak.de>
2025-12-09 11:04:27 +0100

    Add volume map manual to index

--- src/resources/views/manualVolumes.blade.php
+++ src/resources/views/manualVolumes.blade.php
@@ -5,3 +5,11 @@
 <p>
     Geo overlays are geo-referenced layers that can be used on maps to filter images.
 </p>
+
+<h4>
+    <a href="{{route('manual-tutorials', ['geo', 'volume-map'])}}">Image Volume Map</a>
+</h4>
+
+<p>
+    Image volume map.
+</p>
