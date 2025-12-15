7f44d6c3ba030a84441a7571f7943c77dac3dc59
Leane Schlundt <lhecht@techfak.de>
2025-12-15 11:16:58 +0100

    Refactor TileSingleOverlay

--- src/Jobs/TileSingleOverlay.php
+++ src/Jobs/TileSingleOverlay.php
@@ -4,13 +4,10 @@ namespace Biigle\Modules\Geo\Jobs;
 
 use File;
 use FileCache;
-use PHPExif\Reader\Reader;
-use PHPExif\Enum\ReaderType;
 use Biigle\FileCache\GenericFile;
 use Biigle\Jobs\TileSingleObject;
 use Biigle\Modules\Geo\GeoOverlay;
 use Jcupitt\Vips\Image as VipsImage;
-use Illuminate\Support\Facades\Storage;
 
 class TileSingleOverlay extends TileSingleObject
 {
@@ -29,6 +26,12 @@ class TileSingleOverlay extends TileSingleObject
      */
     public $genericFile;
 
+    protected $vipsImage;
+
+    protected $exif;
+
+    protected $noDataValue;
+
     /**
      * Create a new job instance.
      *
@@ -36,11 +39,12 @@ class TileSingleOverlay extends TileSingleObject
      *
      * @return void
      */    
-    public function __construct(GeoOverlay $file)
+    public function __construct(GeoOverlay $file, array $exif)
     {
         parent::__construct(config('geo.tiles.overlay_storage_disk'), "{$file->id}/{$file->id}_tiles");
         $this->file = $file;
         $this->tempPath = config('geo.tiles.tmp_dir')."/{$file->id}";
+        $this->exif = $exif;
     }
 
     /**
@@ -68,47 +72,23 @@ class TileSingleOverlay extends TileSingleObject
      */
     public function generateTiles($file, $path)
     {
-        $vipsImage = $this->getVipsImage($path);
-        $missingValue = $this->getExifData($path)['IFD0:GDALNoData'];
+        $this->vipsImage = $this->getVipsImage($path);
+        $this->setNoDataValue();
 
-        $min = $vipsImage->min();
-        // Check if metadata returns truncated no data value
-        $minIsMissingValue = $min != 0 && $missingValue / $min >= 0.999;
-        // Use no data value from file instead of truncated value from exif data
-        $missingValue = $minIsMissingValue ? $min : $missingValue;
+        $min = $this->getMin();
+        $max = $this->vipsImage->max();
 
-        if ($minIsMissingValue || $missingValue <= $min) {
-            // Exclude no data values since they do not represent the minimum
-            $min = $vipsImage
-                ->more($missingValue)
-                ->ifthenelse($vipsImage, 9999, ['blend' => false])
-                ->min();
-        }
-        $max = $vipsImage->max();
+        // Must be called before imageNormalization since pixel values could be changed
+        $alpha = $this->generateAlphaMask();
 
-        $newImage = $vipsImage;
         // If smaller or larger range is used, normalize pixel range to enhance contrast
         if ($min != 0 && $max != 255) {
-            $newImage = $this->imageNormalization($vipsImage, $min, $max);
+            $this->vipsImage = $this->imageNormalization($min, $max);
         }
 
-        // Check whether image contains no data values
-        $hasMissingValues = $vipsImage->hist_find()->writeToArray()[$missingValue] > 0;
-        if ($hasMissingValues) {
-            // Create alpha mask
-            $alpha = $vipsImage->equal($missingValue)->ifthenelse(0, 255, ['blend' => false]);
+        $this->vipsImage = $this->maybeAddAlpha($alpha);
 
-            // Set transparecy to 0 if pixel is a no data value
-            if ($newImage->bands === 4) {
-                // discard old alpha channel
-                $newImage = $newImage->extract_band(0, ['n' => 3]);
-                $newImage = $newImage->bandjoin($alpha);
-            } else {
-                $newImage = $newImage->bandjoin($alpha);
-            }
-        }
-
-        $newImage->dzsave($this->tempPath, [
+        $this->vipsImage->dzsave($this->tempPath, [
             'layout' => 'zoomify',
             'container' => 'fs',
             'strip' => true,
@@ -117,10 +97,107 @@ class TileSingleOverlay extends TileSingleObject
         ]);
     }
 
-    protected function getExifData($path)
+    /**
+     *
+     * Get the image's minimum by ignoring the no data values if they are present
+     *
+     * @return float
+     */
+    protected function getMin()
     {
-        $reader = Reader::factory(ReaderType::EXIFTOOL);
-        return $reader->read($path)->getRawData();
+        $min = $this->vipsImage->min();
+
+        if (is_null($this->noDataValue)) {
+            return $min;
+        }
+
+        if ($this->minimumEqualsNoDataValue() || $this->noDataValue < $min) {
+            // Exclude no data values since they do not represent the minimum
+            $min = $this->vipsImage
+                ->more($this->noDataValue)
+                ->ifthenelse($this->vipsImage, 9999, ['blend' => false])
+                ->min();
+        }
+
+        return $min;
+    }
+
+    /**
+     * Set the no data value
+     *
+     * @return void
+     */
+    protected function setNoDataValue()
+    {
+        $this->noDataValue = array_key_exists('IFD0:GDALNoData', $this->exif) ? $this->exif['IFD0:GDALNoData'] : null;
+
+        if (is_null($this->noDataValue)) {
+            return;
+        }
+
+        // If image's minimum equals the no data value,
+        // return the minimum because otherwise comparisons with the no data value will not work
+        $this->noDataValue = $this->minimumEqualsNoDataValue() ? $this->vipsImage->min() : $this->noDataValue;
+    }
+
+    /**
+     * Checks if image's min and no data value is (almost) equal
+     *
+     * @return bool
+     */
+    protected function minimumEqualsNoDataValue()
+    {
+        if (is_null($this->noDataValue)) {
+            return false;
+        }
+
+        $min = $this->vipsImage->min();
+        return $min != 0 && $this->noDataValue / $min >= 0.999 || $min === $this->noDataValue;
+    }
+
+    /**
+     * Generates an alpha mask for images containing the no data value.
+     * Otherwise, return null.
+     *
+     * @return VipsImage|null
+     */
+    protected function generateAlphaMask()
+    {
+        if (is_null($this->noDataValue)) {
+            return;
+        }
+
+        $hasMissingValues = $this->vipsImage->hist_find()->writeToArray()[$this->noDataValue] > 0;
+        // Create alpha mask if image contains noData values
+        return $hasMissingValues ?
+            $this->vipsImage->equal($this->noDataValue)->ifthenelse(0, 255, ['blend' => false]) :
+            null;
+    }
+
+    /**
+     * Adds the alpha mask to an image if the image contains no data values
+     *
+     * @param mixed $alpha
+     *
+     * @return VipsImage
+     */
+    protected function maybeAddAlpha($alpha)
+    {
+        if (is_null($alpha)) {
+            return $this->vipsImage;
+        }
+
+        $image = null;
+        // Set transparency to 0 if pixel is a no data value
+        if ($this->vipsImage->bands === 4) {
+            // replace old alpha channel in RGBA image
+            $this->vipsImage->extract_band(0, ['n' => 3]);
+            $image = $this->vipsImage->bandjoin($alpha);
+        } else {
+            $image = $this->vipsImage->bandjoin($alpha);
+        }
+
+        return $image;
     }
 
     /**
@@ -139,16 +216,15 @@ class TileSingleOverlay extends TileSingleObject
      * Normalize the image band to 0 to 255 to enhance contrast
      * since some images use only a very small pixel range.
      *
-     * @param \Jcupitt\Vips\Image $vipsImage
      * @param float $min minimum value of color-level of the input image
      * @param float $max maximum value of color-level of the input image
      * 
      * @return \Jcupitt\Vips\Image
      */
-    public function imageNormalization($vipsImage, $min, $max)
+    public function imageNormalization($min, $max)
     {
         // band intensity normalization x' = (x - $min) / ($max - $min) * 255
-        return $vipsImage
+        return $this->vipsImage
             ->subtract($min)
             ->multiply(255 / ($max - $min))
             ->cast('uchar');
