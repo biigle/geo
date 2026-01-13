5b294dc164fe8a48499176f1694286eb554a4eca
Leane Schlundt <lhecht@techfak.de>
2026-01-13 08:01:28 +0100

    Add php doc

--- src/Http/Controllers/Api/WebMapOverlayController.php
+++ src/Http/Controllers/Api/WebMapOverlayController.php
@@ -14,6 +14,33 @@ class WebMapOverlayController extends Controller
     /**
      * Save Webmap as GeoOverlay
      *
+     * @api {post} volumes/{id}/geo-overlays/webmap Save webmap to create a geo overlay
+     * @apiGroup Geo
+     * @apiName VolumesStoreWMS
+     * @apiPermission projectAdmin
+     *
+     * @apiParam {String} Url to the webmap source
+     *
+     * @apiParamExample {String} Request example:
+     * url: https://example.com
+     *
+     * @apiSuccessExample {json} Success response:
+     * {
+     * "id" => 1
+     * "name" => "Title_0"
+     * "type" => "webmap"
+     * "browsing_layer" => true
+     * "layer_index" => null
+     *   "attrs" => {
+     *      "top_left_lng" => "-2.9198048485707",
+     *      "top_left_lat" => "57.0974838573358",
+     *      "bottom_right_lng" => "-2.9170062535908",
+     *      "bottom_right_lat" => "57.0984589659731",
+     *      "url" => https://example.com,
+     *      "layers" => [ 0 => "Layer_1" ]
+     *      }
+     * }
+     *
      * @param StoreWebMapOverlay $request
      * @return GeoOverlay
      */
