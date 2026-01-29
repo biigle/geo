e7ebfa9419a07f9b9e74a681920fe8b16e6a6af3
Leane Schlundt <lhecht@techfak.de>
2026-01-29 08:44:26 +0100

    Refactor code

--- src/Jobs/TileSingleOverlay.php
+++ src/Jobs/TileSingleOverlay.php
@@ -45,13 +45,6 @@ class TileSingleOverlay extends TileSingleObject
     protected $exif;
 
     /**
-     * Value representing missing data
-     *
-     * @var int|float
-     */
-    protected $noDataValue;
-
-    /**
      * User of the request
      *
      * @var User
@@ -101,22 +94,29 @@ class TileSingleOverlay extends TileSingleObject
     public function generateTiles($file, $path)
     {
         $this->vipsImage = $this->getVipsImage($path);
-
         $this->validateImage();
-        $this->setNoDataValue();
 
-        $min = $this->getMin();
+        $min = $this->vipsImage->min();
+        $noDataValue = $this->exif['IFD0:GDALNoData'] ?? null;
+
+        // Check if the minimum is the real minimum and not the noDataValue
+        if ($noDataValue !== null) {
+            $minIsNoDataValue = $noDataValue == $min || $min != 0 && $noDataValue / $min >= 0.999;
+            // If image's minimum equals the no data value,
+            // return the minimum because otherwise comparisons with the no data value would not work
+            $noDataValue = $minIsNoDataValue ? $min : $noDataValue;
+            $min = $this->getNextMin($min, $noDataValue, $minIsNoDataValue);
+        }
+
         $max = $this->vipsImage->max();
-        $isBlackImg = (int) $min === 0 && (int) $max === 0;
+        $isBlackImg = $min == 0 && $max == 0;
 
         if (!$isBlackImg && $this->vipsImage->bands === 1) {
             // Must be called before imageNormalization since pixel values could be changed
-            $alpha = $this->maybeGenerateAlphaMask();
+            $alpha = $this->maybeGenerateAlphaMask($noDataValue);
 
-            // If smaller or larger range is used, normalize pixel range to enhance contrast
-            if ($min != 0 || $max != 255) {
-                $this->vipsImage = $this->imageNormalization($min, $max);
-            }
+            // If a smaller or larger range is used, normalize the pixel range to the enhance contrast
+            $this->vipsImage = $this->imageNormalization($min, $max);
 
             $this->vipsImage = $this->maybeAddAlpha($alpha);
 
@@ -157,57 +157,17 @@ class TileSingleOverlay extends TileSingleObject
      *
      * @return float
      */
-    protected function getMin()
+    protected function getNextMin($min, $noDataValue, $minIsNoDataValue)
     {
-        $min = $this->vipsImage->min();
-
-        if (is_null($this->noDataValue)) {
+        if (!$minIsNoDataValue && $noDataValue >= $min) {
             return $min;
         }
 
-        if ($this->minimumEqualsNoDataValue() || $this->noDataValue < $min) {
-            // Exclude no data values since they do not represent the minimum
-            $min = $this->vipsImage
-                ->more($this->noDataValue)
-                ->ifthenelse($this->vipsImage, 9999, ['blend' => false])
-                ->min();
-        }
-
-        return $min;
-    }
-
-    /**
-     * Set the no data value
-     *
-     * @return void
-     */
-    protected function setNoDataValue()
-    {
-        $this->noDataValue = array_key_exists('IFD0:GDALNoData', $this->exif) ? $this->exif['IFD0:GDALNoData'] : null;
-
-        if (is_null($this->noDataValue)) {
-            return;
-        }
-
-        // If image's minimum equals the no data value,
-        // return the minimum because otherwise comparisons with the no data value would not work
-        $this->noDataValue = $this->minimumEqualsNoDataValue() ? $this->vipsImage->min() : $this->noDataValue;
-    }
-
-    /**
-     * Checks if image's min and no data value is (almost) equal
-     *
-     * @return bool
-     */
-    protected function minimumEqualsNoDataValue()
-    {
-        if (is_null($this->noDataValue)) {
-            return false;
-        }
-
-        $min = $this->vipsImage->min();
-        // Check if values are almost equal, because sometimes the noDataValue is the truncated minimum
-        return $this->noDataValue == $min || $min != 0 && $this->noDataValue / $min >= 0.999;
+        // Exclude no data values since they do not represent the minimum
+        return $this->vipsImage
+            ->more($noDataValue)
+            ->ifthenelse($this->vipsImage, 9999, ['blend' => false])
+            ->min();
     }
 
     /**
@@ -216,16 +176,16 @@ class TileSingleOverlay extends TileSingleObject
      *
      * @return VipsImage|null
      */
-    protected function maybeGenerateAlphaMask()
+    protected function maybeGenerateAlphaMask($noDataValue)
     {
-        if (is_null($this->noDataValue)) {
+        if ($noDataValue === null) {
             return;
         }
 
-        $hasMissingValues = $this->vipsImage->hist_find()->writeToArray()[$this->noDataValue] > 0;
+        $hasMissingValues = $this->vipsImage->hist_find()->writeToArray()[$noDataValue] > 0;
         // Create alpha mask if image contains noData values
         return $hasMissingValues ?
-            $this->vipsImage->equal($this->noDataValue)->ifthenelse(0, 255, ['blend' => false]) :
+            $this->vipsImage->equal($noDataValue)->ifthenelse(0, 255, ['blend' => false]) :
             null;
     }
 
@@ -238,7 +198,7 @@ class TileSingleOverlay extends TileSingleObject
      */
     protected function maybeAddAlpha($alpha)
     {
-        if (is_null($alpha)) {
+        if ($alpha === null) {
             return $this->vipsImage;
         }
 
@@ -271,6 +231,10 @@ class TileSingleOverlay extends TileSingleObject
      */
     protected function imageNormalization($min, $max)
     {
+        if ($min == 0 && $max == 255) {
+            return $this->vipsImage;
+        }
+
         // band intensity normalization x' = (x - $min) / ($max - $min) * 255
         return $this->vipsImage
             ->subtract($min)
