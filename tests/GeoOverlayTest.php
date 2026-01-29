a08a6ab4a8e4995636dca80d4c30ab089379d70c
Leane Schlundt <lhecht@techfak.de>
2026-01-29 09:49:51 +0100

    Add tests

--- tests/GeoOverlayTest.php
+++ tests/GeoOverlayTest.php
@@ -3,6 +3,7 @@
 namespace Biigle\Tests\Modules\Geo;
 
 use Biigle\Modules\Geo\GeoOverlay;
+use Biigle\Volume;
 use TestCase;
 
 class GeoOverlayTest extends TestCase
@@ -28,6 +29,74 @@ class GeoOverlayTest extends TestCase
         $this->assertNull($webmap->created_at);
         $this->assertNull($webmap->updated_at);
         $this->assertEquals($webmap->type, 'webmap');
+    }
+
+    public function testBuildGeoTiff()
+    {
+        $volume = Volume::factory()->create();
+        $overlay = GeoOverlay::build($volume->id, "test", "geotiff", 0, [
+            [-2.9213164328107, 57.096651484989, -2.9185182540292, 57.097626526122],
+            [3, 2]
+        ]);
+
+        $this->assertEquals($volume->id, $overlay->volume_id);
+        $this->assertEquals($overlay->type, 'geotiff');
+        $this->assertEquals($overlay->name, 'test');
+        $this->assertEquals($overlay->layer_index, 0);
+        $this->assertEquals(-2.9213164328107, $overlay->attrs['top_left_lng']);
+        $this->assertEquals(57.096651484989, $overlay->attrs['top_left_lat']);
+        $this->assertEquals(-2.9185182540292, $overlay->attrs['bottom_right_lng']);
+        $this->assertEquals(57.097626526122, $overlay->attrs['bottom_right_lat']);
+        $this->assertNull($overlay->created_at);
+        $this->assertNull($overlay->updated_at);
+        $this->assertEquals(3, $overlay->attrs['width']);
+        $this->assertEquals(2, $overlay->attrs['height']);
+    }
+
+    public function testBuildWebMap()
+    {
+        $volume = Volume::factory()->create();
+        $overlay = GeoOverlay::build($volume->id, "test", "webmap", 0, [
+            [-2.9213164328107, 57.096651484989, -2.9185182540292, 57.097626526122],
+            'https://example.com',
+            'test'
+        ]);
+
+        $this->assertEquals($volume->id, $overlay->volume_id);
+        $this->assertEquals($overlay->type, 'webmap');
+        $this->assertEquals($overlay->name, 'test');
+        $this->assertEquals($overlay->layer_index, 0);
+        $this->assertEquals(-2.9213164328107, $overlay->attrs['top_left_lng']);
+        $this->assertEquals(57.096651484989, $overlay->attrs['top_left_lat']);
+        $this->assertEquals(-2.9185182540292, $overlay->attrs['bottom_right_lng']);
+        $this->assertEquals(57.097626526122, $overlay->attrs['bottom_right_lat']);
+        $this->assertNull($overlay->created_at);
+        $this->assertNull($overlay->updated_at);
+        $this->assertEquals('https://example.com', $overlay->attrs['url']);
+        $this->assertEquals('test', $overlay->attrs['layer']);
+    }
+
+    public function testBuildWebMapWithoutCoords()
+    {
+        $volume = Volume::factory()->create();
+        $overlay = GeoOverlay::build($volume->id, "test", "webmap", 0, [
+            [],
+            'https://example.com',
+            'test'
+        ]);
+
+        $this->assertEquals($volume->id, $overlay->volume_id);
+        $this->assertEquals($overlay->type, 'webmap');
+        $this->assertEquals($overlay->name, 'test');
+        $this->assertEquals($overlay->layer_index, 0);
+        $this->assertArrayNotHasKey('top_left_lng', $overlay->attrs);
+        $this->assertArrayNotHasKey('top_left_lat', $overlay->attrs);
+        $this->assertArrayNotHasKey('bottom_right_lng', $overlay->attrs);
+        $this->assertArrayNotHasKey('bottom_right_lat', $overlay->attrs);
+        $this->assertNull($overlay->created_at);
+        $this->assertNull($overlay->updated_at);
+        $this->assertEquals('https://example.com', $overlay->attrs['url']);
+        $this->assertEquals('test', $overlay->attrs['layer']);
     }
 
     public function testVolumeOnDeleteCascade()
